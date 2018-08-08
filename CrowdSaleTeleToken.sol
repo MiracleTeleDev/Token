95cc1de2cb6c7b3404a38d49b688c4fa8e821f5d
Miracle Dev <dev@miracletele.com>
2018-08-08 15:39:36 +0200

    add withdrawToken method, format code

--- CrowdSaleTeleToken.sol
+++ CrowdSaleTeleToken.sol
@@ -7,66 +7,79 @@ import "browser/SafeMath.sol";
 
 contract CrowdSaleTeleToken is Owned {
 
-    using SafeMath for uint256;
+	using SafeMath for uint256;
 
-    uint256 public price;
+	uint256 public price;
 
-    ERC20Token public crowdSaleToken;
+	ERC20Token public crowdSaleToken;
 
-    /**
-     * Constructor function, setup token
-     */
-    constructor(uint256 _price, address _tokenAddress)
-    	public
-    {
-        //set initial token price
-        price = _price;
+	/**
+	 * Constructor function
+	 *
+	 * Setup the owner
+	 */
+	constructor(uint256 _price, address _tokenAddress)
+		public
+	{
+		//set initial token price
+		price = _price;
 
-        //set crowdsale token
-        crowdSaleToken = ERC20Token(_tokenAddress);
-    }
+		//set crowdsale token
+		crowdSaleToken = ERC20Token(_tokenAddress);
+	}
 
-    /**
-     * Fallback function
-     *
-     * The function without name is the default function that is called whenever anyone sends funds to a contract
-     */
-    function ()
-    	payable
-    	public
-    {
-        //calc buy amount
-	uint256 amount = msg.value / price;
+	/**
+	 * Fallback function
+	 *
+	 * The function without name is the default function that is called whenever anyone sends funds to a contract
+	 */
+	function ()
+		payable
+		public
+	{
+		//calc buy amount
+		uint256 amount = msg.value / price;
 
-	//check amount, it cannot be zero
-	require(amount != 0);
+		//check amount, it cannot be zero
+		require(amount != 0);
 
-        //transfer required amount
-	crowdSaleToken.transfer(msg.sender, amount.mul(10**uint256(crowdSaleToken.decimals)));
-    }
+		//transfer required amount
+		crowdSaleToken.transfer(msg.sender, amount.mul(10**uint256(crowdSaleToken.decimals)));
+	}
 
-    /**
-     * Withdraw the funds
-     */
-    function withdrawal(uint256 _amount)
-    	public
-    	onlyOwner
-    {
-        //send requested amount to owner
-    	msg.sender.transfer(_amount);
-    }
+	/**
+	 * Withdraw eth
+	 */
+	function withdrawalEth(uint256 _amount)
+		public
+		onlyOwner
+	{
+		//send requested amount to owner
+		msg.sender.transfer(_amount);
+	}
 
-    /**
-     * Set token price
-     */
-    function setPrice(uint256 _price)
-    	public
-    	onlyOwner
-    {
-	//check new price, it cannot be zero
-	assert(_price != 0);
+	/**
+	 * Withdraw tokens
+	 */
+	function withdrawalToken(uint256 _amount)
+		public
+		onlyOwner
+	{
+		//send requested amount to owner
+		crowdSaleToken.transfer(msg.sender, _amount);
+	}
 
-	//set new crowdsale token price
-	price = _price;
-    }
+	/**
+	 * Set token price
+	 */
+	function setPrice(uint256 _price)
+		public
+		onlyOwner
+	{
+		//check new price, it cannot be zero
+		assert(_price != 0);
+
+		//set new crowdsale token price
+		price = _price;
+	}
 }
